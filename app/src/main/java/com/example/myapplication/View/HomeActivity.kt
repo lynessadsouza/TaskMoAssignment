3f59e85c2a546142140fa06c08d75bb6d0dc69d7
lynessadsouza <lynessadsouza@gmail.com>
2022-07-06 22:24:22 +0530

    Basic UI

--- app/src/main/java/com/example/myapplication/View/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/View/HomeActivity.kt
@@ -1,14 +1,11 @@
-package com.example.myapplication
-
+package com.example.myapplication.View
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.R
 
-class MainActivity : AppCompatActivity() {
-
-
-
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
