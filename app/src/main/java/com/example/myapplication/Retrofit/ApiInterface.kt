fdb6352b9f2300ac0e83df05b095f9e7304e5119
lynessadsouza <lynessadsouza@gmail.com>
2022-07-07 18:57:12 +0530

    UI Changes

--- app/src/main/java/com/example/myapplication/Retrofit/ApiInterface.kt
+++ app/src/main/java/com/example/myapplication/Retrofit/ApiInterface.kt
@@ -4,8 +4,8 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface ApiInterface {
- @GET("k_qk9qiyxa/ls004285275")
+ //@GET("k_qk9qiyxa/ls004285275")
+ @GET("k_66aq68k3/ls004285275")
      fun getMovies(): Call<MoviesResponse>
-
 }
 //https://imdb-api.com/en/API/IMDbList/k_qk9qiyxa/ls004285275
